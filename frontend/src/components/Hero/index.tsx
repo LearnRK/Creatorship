4fb3216731eb10db10352dac1513ee96698e07be
LearnRK <145103860+LearnRK@users.noreply.github.com>
2024-08-09 20:08:39 +0530

    Update index.tsx

--- frontend/src/components/Hero/index.tsx
+++ frontend/src/components/Hero/index.tsx
@@ -19,10 +19,10 @@ const Hero = () => {
                 </p>
                 <div className="flex flex-col items-center justify-center space-y-4 sm:flex-row sm:space-x-4 sm:space-y-0">
                   <Link
-                    href="https://github.com/rk-astra"
+                    href="/signin"
                     className="rounded-sm bg-primary px-8 py-4 text-base font-semibold text-white duration-300 ease-in-out hover:bg-primary/80"
                   >
-                    Learn more
+                    explore
                   </Link>
                 </div>
               </div>
